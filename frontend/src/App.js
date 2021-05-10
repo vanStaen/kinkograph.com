0de7884bf7f5563f494e7ac3d41ad39ad6e98df5
Clément van Staen <clement.vanstaen@gmail.com>
2021-05-10 15:12:38 +0200

    styling app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
     <div className="App__main">
-      Kinkograph <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <div className="App__title">&nbsp;kinkograph</div> 
     </div>
     </div>
   );
