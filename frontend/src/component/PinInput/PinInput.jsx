b761632d965f9c6da771be223b773f8abb0262f1
Clément van Staen <clement.vanstaen@gmail.com>
2021-05-11 10:33:52 +0200

    PinInput Event listener

--- frontend/src/component/PinInput/PinInput.jsx
+++ frontend/src/component/PinInput/PinInput.jsx
@@ -1,51 +1,78 @@
-import { React } from "react";
+import { React, useCallback, useEffect, useState } from "react";
 import "./PinInput.css";
 
 export const PinInput = (props) => {
-  const inputHandler = (e) => {
-    const nextInput = parseInt(e.target.id, 10) + 1;
-    if (nextInput < 7) {
-      document.getElementById(e.target.id).blur();
-      document.getElementById(nextInput).value = "";
-      document.getElementById(nextInput).focus();
+  const [lastFiledInput, setLastFiledInput] = useState(1);
+
+  const keyDownListener = useCallback((event) => {
+    event.preventDefault();
+    const keyPressed = event.key.toLowerCase();
+    if (keyPressed === "backspace") {
+      if (lastFiledInput > 1) {
+        document.getElementById(lastFiledInput - 1).value = "";
+        setLastFiledInput(lastFiledInput - 1);
+      }
+    } else if (keyPressed.length > 1) {
+      // console.log(keyPressed);
     } else {
-      document.getElementById(1).focus();
-      const code =
-        document.getElementById(1).value +
-        document.getElementById(2).value +
-        document.getElementById(3).value +
-        document.getElementById(4).value +
-        document.getElementById(5).value +
-        document.getElementById(6).value;
-      document.getElementById(1).value = "";
-      document.getElementById(2).value = "";
-      document.getElementById(3).value = "";
-      document.getElementById(4).value = "";
-      document.getElementById(5).value = "";
-      document.getElementById(6).value = "";
-      props.login(code);
+      document.getElementById(lastFiledInput).value = keyPressed;
+      setLastFiledInput(lastFiledInput + 1);
+      if (lastFiledInput + 1 === 7) {
+        setLastFiledInput(1);
+        const code =
+          document.getElementById(1).value +
+          document.getElementById(2).value +
+          document.getElementById(3).value +
+          document.getElementById(4).value +
+          document.getElementById(5).value +
+          document.getElementById(6).value;
+        setTimeout(() => {
+          document.getElementById(1).focus();
+          document.getElementById(1).value = "";
+          document.getElementById(2).value = "";
+          document.getElementById(3).value = "";
+          document.getElementById(4).value = "";
+          document.getElementById(5).value = "";
+          document.getElementById(6).value = "";
+          setLastFiledInput(1);
+        }, 1000);
+        props.login(code);
+      }
     }
-  };
+  });
 
   const clickHandler = (e) => {
     const inputID = parseInt(e.target.id, 10);
-    let lastInputWithData = 6;
+    let lastInputWithData = lastFiledInput;
     if (document.getElementById(1).value === "") {
       lastInputWithData = 1;
+      setLastFiledInput(lastInputWithData);
     } else if (document.getElementById(2).value === "") {
       lastInputWithData = 2;
+      setLastFiledInput(lastInputWithData);
     } else if (document.getElementById(3).value === "") {
       lastInputWithData = 3;
+      setLastFiledInput(lastInputWithData);
     } else if (document.getElementById(4).value === "") {
       lastInputWithData = 4;
+      setLastFiledInput(lastInputWithData);
     } else if (document.getElementById(5).value === "") {
       lastInputWithData = 5;
+      setLastFiledInput(lastInputWithData);
     } else if (document.getElementById(6).value === "") {
       lastInputWithData = 6;
+      setLastFiledInput(lastInputWithData);
     }
     document.getElementById(inputID).blur();
     document.getElementById(lastInputWithData).focus();
   };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownListener);
+    return () => {
+      document.removeEventListener("keydown", keyDownListener);
+    };
+  }, [keyDownListener]);
 
   return (
     <div>
@@ -57,7 +84,6 @@ export const PinInput = (props) => {
           maxLength="1"
           min="1"
           max="1"
-          onChange={inputHandler}
           style={{ marginLeft: "20px" }}
           autoComplete="new-password"
         />
@@ -68,7 +94,6 @@ export const PinInput = (props) => {
           maxLength="1"
           min="1"
           max="1"
-          onChange={inputHandler}
           onClick={clickHandler}
           autoComplete="new-password"
         />
@@ -79,7 +104,6 @@ export const PinInput = (props) => {
           maxLength="1"
           min="1"
           max="1"
-          onChange={inputHandler}
           onClick={clickHandler}
           autoComplete="new-password"
         />
@@ -90,7 +114,6 @@ export const PinInput = (props) => {
           maxLength="1"
           min="1"
           max="1"
-          onChange={inputHandler}
           onClick={clickHandler}
           autoComplete="new-password"
         />
@@ -101,7 +124,6 @@ export const PinInput = (props) => {
           maxLength="1"
           min="1"
           max="1"
-          onChange={inputHandler}
           onClick={clickHandler}
           autoComplete="new-password"
         />
@@ -112,7 +134,6 @@ export const PinInput = (props) => {
           maxLength="1"
           min="1"
           max="1"
-          onChange={inputHandler}
           onClick={clickHandler}
           style={{ width: "40px" }}
           autoComplete="new-password"
