4db336579c476e4dec12d78f34ad98d47289d186
Clément van Staen <clement.vanstaen@gmail.com>
2021-05-14 21:35:53 +0200

    gallery final formating

--- frontend/src/pages/Gallery/Gallery.jsx
+++ frontend/src/pages/Gallery/Gallery.jsx
@@ -3,11 +3,18 @@ import "./Gallery.css";
 export const Gallery = () => {
   return (
     <div className="grid">
-      <div className="item item__landscape"></div>
-      <div className="item item__square"></div>
-      <div className="item item__full"></div>
-      <div className="item item__portrait"></div>
-      <div className="item item__square"></div>
+      <div className="item item__landscape">item__landscape</div>
+      <div className="item item__square">item__square</div>
+      <div className="item item__portrait">item__portrait</div>
+      <div className="item item__landscape">item__landscape</div>
+      <div className="item item__square">item__square</div>
+      <div className="item item__portrait">item__portrait</div>
+      <div className="item item__landscape">item__landscape</div>
+      <div className="item item__square">item__square</div>
+      <div className="item item__portrait">item__portrait</div>
+      <div className="item item__landscape">item__landscape</div>
+      <div className="item item__square">item__square</div>
+      <div className="item item__portrait">item__portrait</div>
     </div>
   );
 };
