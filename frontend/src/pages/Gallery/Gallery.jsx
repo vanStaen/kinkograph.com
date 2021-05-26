5283bb2befac69f33692c968a24f9b8ade594946
cvanstaen <clement.van.staen@zalando.de>
2021-05-26 17:40:08 +0200

    use thumbnail in gallery

--- frontend/src/pages/Gallery/Gallery.jsx
+++ frontend/src/pages/Gallery/Gallery.jsx
@@ -29,7 +29,7 @@ export const Gallery = () => {
           return (
             <div
               className={`item ${picture.format}`}
-              style={{ backgroundImage: `url("${picture.url_med}")` }}
+              style={{ backgroundImage: `url("${picture.url_thumb}")` }}
             >
               {picture.tag}
             </div>)
