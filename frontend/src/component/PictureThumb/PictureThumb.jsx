1ca3148181450e4367d481e400f9b381b3dda742
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-01 14:43:08 +0200

    deploy frontend Build

--- frontend/src/component/PictureThumb/PictureThumb.jsx
+++ frontend/src/component/PictureThumb/PictureThumb.jsx
@@ -1,28 +1,34 @@
 import React, { useState, Fragment } from "react";
+import { EyeOutlined } from "@ant-design/icons";
 
-import { EditDrawer } from '../EditDrawer/EditDrawer';
-import './PictureThumb.css';
+import { EditDrawer } from "../EditDrawer/EditDrawer";
+import "./PictureThumb.css";
 
 export const PictureThumb = (props) => {
-    const [showDrawer, setShowDrawer] = useState(false);
+  const [showDrawer, setShowDrawer] = useState(false);
 
-    return (
-        <Fragment>
-            <EditDrawer
-                picture={props.picture}
-                showDrawer={showDrawer}
-                setShowDrawer={setShowDrawer}
-                reload={props.reload}
-            />
-            <div className="picture__container"
-                onClick={() => setShowDrawer(true)}>
-                <img
-                    className={`picture ${!props.picture.tags && "picture__bluryGray"}`}
-                    src={props.picture.url_thumb}
-                    alt={props.picture.id}
-                    key={props.picture.id}
-                />
-                <div className="picture__tagMissing">{!props.picture.tags && "TAGS MISSING"}</div>
-            </div>
-        </Fragment>)
-} 
+  return (
+    <Fragment>
+      <EditDrawer
+        picture={props.picture}
+        showDrawer={showDrawer}
+        setShowDrawer={setShowDrawer}
+        reload={props.reload}
+      />
+      <div className="picture__container" onClick={() => setShowDrawer(true)}>
+        <img
+          className={`picture ${!props.picture.tags && "picture__bluryGray"}`}
+          src={props.picture.url_thumb}
+          alt={props.picture.id}
+          key={props.picture.id}
+        />
+        <div className="picture__tagMissing">
+          {!props.picture.tags && "TAGS MISSING"}
+        </div>
+        <div className="picture__eye">
+          <EyeOutlined />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
