1ed972fc018cf1126df8ea36890ae61faa90f206
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-01 15:03:18 +0200

    Work on thumb

--- frontend/src/component/PictureThumb/PictureThumb.jsx
+++ frontend/src/component/PictureThumb/PictureThumb.jsx
@@ -1,11 +1,25 @@
 import React, { useState, Fragment } from "react";
-import { EyeOutlined } from "@ant-design/icons";
+import { FullscreenOutlined } from "@ant-design/icons";
 
 import { EditDrawer } from "../EditDrawer/EditDrawer";
 import "./PictureThumb.css";
 
 export const PictureThumb = (props) => {
   const [showDrawer, setShowDrawer] = useState(false);
+
+  const mouseHoverHandler = (hover) => {
+    const element = document.getElementById(`tag_${props.picture.id}`);
+    const picture = document.getElementById(`pic_${props.picture.id}`);
+    if (hover) {
+      element.style.visibility = "visible";
+      element.style.opacity = 1;
+      picture.style.filter = "brightness(50%) grayscale(1)";
+    } else {
+      element.style.visibility = "hidden";
+      element.style.opacity = 0;
+      picture.style.filter = "brightness(100%) grayscale(0)";
+    }
+  };
 
   return (
     <Fragment>
@@ -15,8 +29,14 @@ export const PictureThumb = (props) => {
         setShowDrawer={setShowDrawer}
         reload={props.reload}
       />
-      <div className="picture__container" onClick={() => setShowDrawer(true)}>
+      <div
+        className="picture__container"
+        onClick={() => setShowDrawer(true)}
+        onMouseEnter={() => mouseHoverHandler(true)}
+        onMouseLeave={() => mouseHoverHandler(false)}
+      >
         <img
+          id={`pic_${props.picture.id}`}
           className={`picture ${!props.picture.tags && "picture__bluryGray"}`}
           src={props.picture.url_thumb}
           alt={props.picture.id}
@@ -25,8 +45,8 @@ export const PictureThumb = (props) => {
         <div className="picture__tagMissing">
           {!props.picture.tags && "TAGS MISSING"}
         </div>
-        <div className="picture__eye">
-          <EyeOutlined />
+        <div id={`tag_${props.picture.id}`} className="picture__tagShow">
+          <FullscreenOutlined />
         </div>
       </div>
     </Fragment>
