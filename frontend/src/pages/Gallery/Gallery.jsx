5d1ca278c2da05579ba316e299d629a9d3ad3e5c
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-02 11:42:15 +0200

    page navigation with arrow keys

--- frontend/src/pages/Gallery/Gallery.jsx
+++ frontend/src/pages/Gallery/Gallery.jsx
@@ -20,17 +20,18 @@ const PAGE_SIZE = 100;
 export const Gallery = observer(() => {
   const [isLoading, setIsLoading] = useState(true);
   const [pictures, setPictures] = useState([]);
+  const throttling = useRef(false);
   const pageNumber = useRef(1);
   const lastPageReached = useRef(false);
 
-  const loadImage = image => {
+  const loadImage = (image) => {
     return new Promise((resolve, reject) => {
       const loadImg = new Image();
       loadImg.src = image.url_thumb;
       loadImg.onload = () => resolve(image.url);
-      loadImg.onerror = err => reject(err);
-    })
-  }
+      loadImg.onerror = (err) => reject(err);
+    });
+  };
 
   const fetchPictures = useCallback(async () => {
     try {
@@ -40,7 +41,7 @@ export const Gallery = observer(() => {
       } else {
         lastPageReached.current = false;
       }
-      await Promise.all(pictures.map(picture => loadImage(picture)));
+      await Promise.all(pictures.map((picture) => loadImage(picture)));
       overlayStore.setAllPictures(pictures);
       setPictures(pictures);
     } catch (err) {
@@ -66,58 +67,79 @@ export const Gallery = observer(() => {
     }
   };
 
+  const keyDownHandler = useCallback((event) => {
+    event.preventDefault();
+    const keyPressed = event.key.toLowerCase();
+    if (throttling.current === false) {
+      throttling.current = true;
+      if (keyPressed === "arrowright" && !lastPageReached.current) {
+        nextPageHandler(true);
+      } else if (keyPressed === "arrowleft" && pageNumber.current > 1) {
+        nextPageHandler(false);
+      }
+      setTimeout(() => {
+        throttling.current = false;
+      }, 100);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler);
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [keyDownHandler]);
+
   return (
     <div>
       {isLoading ? (
         <div className="App__flex">
           <LoadingOutlined className="Gallery__spinner" />
-          <div className="gallery__spinnerText">
-            Loading <br />Page {pageNumber.current}
-          </div>
+          <div className="gallery__spinnerText">Loading</div>
         </div>
       ) : (
-          <Fragment>
-            {overlayStore.showOverlay && <GalleryOverlay />}
-            <div className="gallery">
-              <div className="gallery__main">
-                {pictures.map((picture, index) => {
-                  return (
-                    <PictureThumb
-                      picture={picture}
-                      reload={fetchPictures}
-                      key={picture.id}
-                    />
-                  );
-                })}
-              </div>
-              <div className="gallery__next">
-                <div className="gallery__nextTextContainer">
-                  {pageNumber.current === 1 ? (
-                    "Previous"
-                  ) : (
-                      <span
-                        className="gallery__nextText"
-                        onClick={() => nextPageHandler(false)}
-                      >
-                        Previous
-                      </span>
-                    )}
-                  {" | "}
-                  {lastPageReached.current ? (
-                    "Next"
-                  ) : (
-                      <span
-                        className="gallery__nextText"
-                        onClick={() => nextPageHandler(true)}
-                      >
-                        Next
-                      </span>
-                    )}
-                </div>
+        <Fragment>
+          {overlayStore.showOverlay && <GalleryOverlay />}
+          <div className="gallery">
+            <div className="gallery__main">
+              {pictures.map((picture, index) => {
+                return (
+                  <PictureThumb
+                    picture={picture}
+                    reload={fetchPictures}
+                    key={picture.id}
+                  />
+                );
+              })}
+            </div>
+            <div className="gallery__next">
+              <div className="gallery__nextTextContainer">
+                {pageNumber.current === 1 ? (
+                  "Previous"
+                ) : (
+                  <span
+                    className="gallery__nextText"
+                    onClick={() => nextPageHandler(false)}
+                  >
+                    Previous
+                  </span>
+                )}
+                {" | "}
+                {lastPageReached.current ? (
+                  "Next"
+                ) : (
+                  <span
+                    className="gallery__nextText"
+                    onClick={() => nextPageHandler(true)}
+                  >
+                    Next
+                  </span>
+                )}
               </div>
             </div>
-          </Fragment>
-        )}
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 });
