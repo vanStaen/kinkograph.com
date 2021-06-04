fc45e14491ce87ca31748da2b948fc94dc3cefa2
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-04 16:08:16 +0200

    New tag/filter endpoint

--- api/tags.js
+++ api/tags.js
@@ -4,15 +4,18 @@ const router = express.Router();
 const { Client } = require("pg");
 
 // Init Postgres
-const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: true })
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; // This bypasses the SSL verification
 
-// Connect to Postgres 
-client.connect(err => {
+// Connect to Postgres
+client.connect((err) => {
   if (err) {
-    console.error('connection error', err.stack)
+    console.error("connection error", err.stack);
   }
-})
+});
 
 // GET all tags
 router.get("/", async (req, res) => {
@@ -26,17 +29,26 @@ router.get("/", async (req, res) => {
   }
 });
 
-
-// POST new tag in DB
-router.post("/", async (req, res) => {
+// POST return tags conresponding to a filter
+router.post("/filter/", async (req, res) => {
   try {
-    const checkIfTagsExist = await client.query(`SELECT * FROM tags WHERE tag_name='${req.body.tag_name}'`);
-    if (checkIfTagsExist.rows.length > 0) {
-      res.status(201).json({ value: 'failed', message: `${req.body.tag_name} was already in the table 'tags'` });
-    } else {
-      await client.query(`INSERT INTO public.tags(tag_name) VALUES ('${req.body.tag_name}');`);
-      res.status(201).json({ value: 'success', message: `${req.body.tag_name} was added to the table 'tags'` });
-    };
+    let filters = "";
+    const arrayFilter = req.body.filter;
+    arrayFilter.forEach(
+        (filter) => (filters = filters + `AND tags LIKE '%"${filter}"%' `)
+      );
+    const selectTagsQuery = `SELECT tags FROM pictures WHERE tags_missing=false ${filters} `;
+    const tagsFilter = await client.query(selectTagsQuery);
+    const allTagsFromFilter = [];
+    tagsFilter.rows.forEach((row) => {
+      const tags = JSON.parse(row.tags);
+      tags.forEach((tag) => {
+        const findIndex = allTagsFromFilter.findIndex(e => e === tag);
+        const isInFilter = arrayFilter.findIndex(e => e === tag);
+        if (findIndex < 0 && isInFilter < 0) {allTagsFromFilter.push(tag)};
+      });
+    });
+    res.status(201).json(allTagsFromFilter);
   } catch (err) {
     res.status(400).json({
       error: `${err})`,
@@ -44,5 +56,35 @@ router.post("/", async (req, res) => {
   }
 });
 
+// POST new tag in DB
+router.post("/", async (req, res) => {
+  try {
+    const checkIfTagsExist = await client.query(
+      `SELECT * FROM tags WHERE tag_name='${req.body.tag_name}'`
+    );
+    if (checkIfTagsExist.rows.length > 0) {
+      res
+        .status(201)
+        .json({
+          value: "failed",
+          message: `${req.body.tag_name} was already in the table 'tags'`,
+        });
+    } else {
+      await client.query(
+        `INSERT INTO public.tags(tag_name) VALUES ('${req.body.tag_name}');`
+      );
+      res
+        .status(201)
+        .json({
+          value: "success",
+          message: `${req.body.tag_name} was added to the table 'tags'`,
+        });
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: `${err})`,
+    });
+  }
+});
 
 module.exports = router;
