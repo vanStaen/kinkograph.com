3e690c0ca5b3f72c3c9b00b54fbf8ef298d41427
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-05 15:36:36 +0200

    Login/User WIP

--- api/login.js
+++ api/login.js
@@ -1,21 +1,131 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
+
+// Check if login data sent are correct
+router.get("/", async (req, res) => {
+  try {
+    let filter = "";
+    if (req.body.email) {
+      filter = `WHERE email=${req.body.email}`;
+    } else if (req.body.username) {
+      filter = `WHERE username=${req.body.username}`;
+    } else {
+      res.status(401).json({
+        error: "Either 'Email' or 'Username' were provided",
+      });
+    }
+    const password = req.body.password;
+    const user = await client.query(`SELECT id, pwd FROM users`);
+
+    if (user.rows < 1) {
+      return res.status(400).json({ error: `User does not exist!` });
+    }
+
+    const isValid = await bcrypt.compare(password, user.rows[0].pwd);
+
+    if (!isValid) {
+      return res.status(400).json({ error: `Password is incorrect!` });
+    }
+
+    const accessToken = await jsonwebtoken.sign(
+      { userId: user.id },
+      process.env.AUTH_SECRET_KEY,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = await jsonwebtoken.sign(
+      { userId: user.id },
+      process.env.AUTH_SECRET_KEY_REFRESH,
+      { expiresIn: "7d" }
+    );
+
+    // TODO
+    /* Add refresh token to db
+    const newToken = {
+      token: refreshToken,
+      userId: user.id
+    }; */
+
+    res.status(200).json({
+      userId: user.id,
+      token: accessToken,
+      refreshToken: refreshToken,
+    });
+
+  } catch (err) {
+    res.status(400).json({
+      error: `${err})`,
+    });
+  }
+});
 
 // Check if code used for Login is correct
-router.get("/:code", async (req, res) => {   
-    try {
-        if (req.params.code === process.env.ACCESS_CODE) {        
-            res.status(201).json("Access Granted");
-        } else {
-            res.status(401).json({
-                error: "Unauthorized",
-            });
-        }    
-    } catch (err) {
-        res.status(400).json({
-            error: `${err})`,
+router.get("/code", async (req, res) => {
+  try {
+    const code = req.body.code;
+    if (code === process.env.ACCESS_CODE) {
+      res.status(201).json({ user: "Guest" });
+    } else {
+      const access = await client.query(
+        `SELECT * FROM users WHERE access_code=${code}`
+      );
+      if (access.rows < 1) {
+        res.status(401).json({
+          error: "User not found",
         });
+      }
+
+      const accessToken = await jsonwebtoken.sign(
+        { userId: user.id },
+        process.env.AUTH_SECRET_KEY,
+        { expiresIn: "15m" }
+      );
+
+      const refreshToken = await jsonwebtoken.sign(
+        { userId: user.id },
+        process.env.AUTH_SECRET_KEY_REFRESH,
+        { expiresIn: "7d" }
+      );
+
+      // TODO
+      /* Add refresh token to db
+    const newToken = {
+      token: refreshToken,
+      userId: user.id
+    }; */
+
+      res.status(200).json({
+        userId: user.id,
+        token: accessToken,
+        refreshToken: refreshToken,
+      });
     }
+
+  } catch (err) {
+    res.status(400).json({
+      error: `${err})`,
+    });
+  }
 });
+
+// DEL Logout
+router.delete("/", async (req, res) => {
+    if (!req.body.refreshToken) {
+      return res.status(401).json({ error: `No refresh token was provided` });
+    }
+    const refreshToken = req.body.refreshToken;
+    
+    //TODO
+    /* const deleteToken = await Token.deleteOne({ token: refreshToken });
+  
+    if (deleteToken.deletedCount === 0) {
+      return res.status(401).json({ error: `Refresh token not found in db!` });
+    }*/
+  
+    // Html resp code 204 return no content
+    res.status(204).json();
+  });
 
 module.exports = router;
