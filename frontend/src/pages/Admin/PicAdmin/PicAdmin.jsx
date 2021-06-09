97b04be5a2e7da8b33b8f349dcda24e6892a54ac
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-09 15:22:04 +0200

    tag mangement in picture admin

--- frontend/src/pages/Admin/PicAdmin/PicAdmin.jsx
+++ frontend/src/pages/Admin/PicAdmin/PicAdmin.jsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect, useCallback, Fragment } from "react";
 import { LoadingOutlined } from "@ant-design/icons";
+import { Select } from "antd";
 
+import { getFilteredTags } from "../../../store/calls/getTags";
 import { getPictures } from "../../../store/calls/getPictures";
 import { EditDrawer } from "../Uploader/EditDrawer/EditDrawer";
+import { capitalizeFirstLetter } from "../../../helpers/capitalizeFirstLetter";
+
 import "./PicAdmin.css";
+
+const { Option } = Select;
 
 export const PicAdmin = () => {
   const [isLoading, setIsloading] = useState(true);
   const [allPictures, setAllPictures] = useState([]);
   const [showDrawer, setShowDrawer] = useState(false);
   const [pictureSelected, setPictureSelected] = useState(null);
+  const [allTags, setAllTags] = useState([]);
+  const [tagSelected, setTagSelected] = useState(null);
 
   const fetchAllPictures = useCallback(async () => {
     try {
       const fetchedPics = await getPictures(0, true);
+      const fetchedTags = await getFilteredTags([]);
       setAllPictures(fetchedPics);
+      setAllTags(fetchedTags);
     } catch (err) {
       console.log(err);
     }
@@ -45,19 +55,45 @@ export const PicAdmin = () => {
           <div className="gallery__spinnerText">loading</div>
         </div>
       ) : (
-        <div className="picAdmin__main">
-          {allPictures.map((picture) => {
-            return (
-              <img
-                id={`pic_${picture.id}`}
-                src={picture.url_thumb}
-                alt={picture.id}
-                key={picture.id}
-                onClick={() => pictureClickHandle(picture)}
-              />
-            );
-          })}
-        </div>
+        <Fragment>
+          <div className="picAdmin__tagSelector">
+            <Select
+              allowClear={false}
+              style={{ width: "100%" }}
+              placeholder="Select a tag to manage"
+              allowClear
+              onChange={(value) => {
+                setTagSelected(value);
+              }}
+            >
+              {allTags.map((tag) => {
+                return (
+                  <Option key={capitalizeFirstLetter(tag.tag)}>
+                    {capitalizeFirstLetter(tag.tag)} ({tag.occur})
+                  </Option>
+                );
+              })}
+            </Select>
+          </div>
+          <div className="picAdmin__main">
+            {allPictures.map((picture) => {
+              return (
+                <img
+                  className={
+                    picture.tags.includes(tagSelected)
+                      ? "picAdmin__tagHalo"
+                      : "picAdmin__tagSepia"
+                  }
+                  id={`pic_${picture.id}`}
+                  src={picture.url_thumb}
+                  alt={picture.id}
+                  key={picture.id}
+                  onClick={() => pictureClickHandle(picture)}
+                />
+              );
+            })}
+          </div>
+        </Fragment>
       )}
     </Fragment>
   );
