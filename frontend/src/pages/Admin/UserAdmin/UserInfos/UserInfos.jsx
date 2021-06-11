aa39f1b7f72f41c7893f60957e113882c615bafd
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-11 10:29:55 +0200

    user overview improvement

--- frontend/src/pages/Admin/UserAdmin/UserInfos/UserInfos.jsx
+++ frontend/src/pages/Admin/UserAdmin/UserInfos/UserInfos.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { Tooltip } from "antd";
+import { HistoryOutlined } from "@ant-design/icons";
+
 import { getFavoritesPictures } from "../../../../store/calls/getFavoritesPictures";
 import "./UserInfos.css";
 
 export const UserInfos = (props) => {
   const [favorites, setFavorites] = useState([]);
+  const [showDetail, setShowDetail] = useState(false);
 
   const fetchFavorites = useCallback(async () => {
     if (props.user.favorites) {
@@ -13,14 +17,35 @@ export const UserInfos = (props) => {
     }
   }, [props.user.favorites]);
 
+  const clickHandler = () => {
+    const showDetailElement = document.getElementById(
+      `userInfos__detail__${props.user.id}`
+    );
+    if (showDetail) {
+      showDetailElement.style.display = "none";
+    } else {
+      showDetailElement.style.display = "block";
+    }
+    setShowDetail(!showDetail);
+  };
+
   useEffect(() => {
     fetchFavorites();
   }, [fetchFavorites]);
 
   return (
-    <div className="userInfos__user" key={`user_${props.user.id}`}>
+    <div
+      className="userInfos__user"
+      key={`user_${props.user.id}`}
+      onClick={clickHandler}
+    >
       <div className="userInfos__title">
         <div className="userInfos__titleLeft">
+          {props.user.last_login && (
+            <Tooltip title={Date(props.user.last_login).toString()}>
+              <HistoryOutlined className="userInfos__lastOnline" />
+            </Tooltip>
+          )}
           {props.user.name.toUpperCase()}
           <span className="smallFont">
             {props.user.access_code && ` ${props.user.access_code} `}
@@ -38,20 +63,26 @@ export const UserInfos = (props) => {
           </span>
         </div>
       </div>
-      {favorites.length > 0 &&
-        favorites.map((fav) => {
-          return (
-            <div className="userInfos__pictureContainer">
-              <div className="smallFont"> #{fav.id}</div>
-              <img
-                className="userInfos__picture"
-                src={fav.url_med}
-                alt={fav.id}
-                key={fav.id}
-              />
-            </div>
-          );
-        })}
+      <div
+        className="userInfos__detail"
+        id={`userInfos__detail__${props.user.id}`}
+      >
+        {favorites.length > 0
+          ? favorites.map((fav) => {
+              return (
+                <div className="userInfos__pictureContainer">
+                  <div className="smallFont"> #{fav.id}</div>
+                  <img
+                    className="userInfos__picture"
+                    src={fav.url_med}
+                    alt={fav.id}
+                    key={fav.id}
+                  />
+                </div>
+              );
+            })
+          : "nothing yet"}
+      </div>
     </div>
   );
 };
