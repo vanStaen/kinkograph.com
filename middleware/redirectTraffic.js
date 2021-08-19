48f7bc0113bcc0fd718e86dfebec3596a9215072
vanStaen <clemenbt.vanstaen@gmail.com>
2021-08-19 14:10:04 +0200

    redirect debugging

--- middleware/redirectTraffic.js
+++ middleware/redirectTraffic.js
@@ -1,5 +1,9 @@
 module.exports = function redirectTraffic(req, res, next) {
+  console.log("middleware redirect")
   if (req.headers.host.slice(0, 4) === "www." || req.protocol === "http") {
+    console.log("req.headers.host", req.headers.host)
+    console.log("req.protocol", req.protocol)
+    console.log("redirected to: https://kinkograph.com")
     return res.redirect(301, 'https://kinkograph.com');
   }
   next();
