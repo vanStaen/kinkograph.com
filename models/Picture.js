b6a1b61fe44cf44b1f2ed456b2bc30301cf1e410
vanStaen <clement.vanstaen@gmail.com>
2022-04-19 13:50:11 +0200

    new models

---
+++ models/Picture.js
@@ -0,0 +1,47 @@
+const { sequelize, DataTypes } = require('../helpers/sequelizedb');
+
+const Picture = sequelize.define("picture", {
+  id: {
+    type: DataTypes.INTEGER,
+    field: "id",
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  url_original: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  url_thumb: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  url_med: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tags: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  original_name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  original_type: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  tags_missing: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+  key: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = {
+  Picture
+};
