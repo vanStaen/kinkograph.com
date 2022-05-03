ca03d0261e90d72de198252c130f25ed05ff176b
vanStaen <clement.vanstaen@gmail.com>
2022-05-03 13:22:17 +0200

    mail service sending name under right name

--- api/service/mailService.js
+++ api/service/mailService.js
@@ -23,7 +23,7 @@ const emailDisclaimer = `
   Be especially wary of .zip or other compressed or executable file types. 
   Do not provide sensitive personal information (like usernames and passwords) over email. 
   Watch for email senders that use suspicious or misleading domain names. 
-  If you can’t tell if an email is legitimate or not, please transfer it to us at <u>info@rewaer.com</u>. 
+  If you can’t tell if an email is legitimate or not, please transfer it to us at <u>info@kinkograph.com</u>. 
   Be especially cautious when opening attachments or clicking links if you receive an email 
   containing a warning banner indicating that it originated from an external source.<br/><br/>
   <b>Virus transmission</b><br/>
@@ -35,7 +35,7 @@ const emailDisclaimer = `
   of the author and do not represent those of the company. No liability can be held 
   for any damages, however, caused, to any recipients of this message. <br/><br/>
   <b>GDPR</b><br/>
-  Rewaer is compliant with the General Data Protection Regulation (GDPR) (EU) 2016/679. 
+  Kinkograph is compliant with the General Data Protection Regulation (GDPR) (EU) 2016/679. 
   We are committed to guaranteeing the security and protection of the private information 
   that we process. To understand more about how we collect, store, and process your personal 
   information in compliance with GDPR, please take a look at our privacy policy
@@ -47,7 +47,7 @@ exports.mailService = {
 
   async mail(sendto, subject, body) {
     const requestBody = {
-      "from": "Rewaer <info@rewaer.com>",
+      "from": "Kinkograph <info@kinkograph.com>",
       "to": sendto,
       "subject": subject,
       "body": `${body}<br/> ${emailDisclaimer}`,
@@ -86,17 +86,17 @@ exports.mailService = {
                   <b>If you did not request this, ignore this email</b> and nothing else will happen.<br/>
                   <br/>
                   This link will only be active for 10 minutes. <br/>
-                  https://rewaer.com/recoverpwd/${recoveryToken}<br/>
+                  https://kinkograph.com/recoverpwd/${recoveryToken}<br/>
                   <br/>
-                  Rewaer App<br/>
+                  Kinkograph App<br/>
                   <i>The Fashion App for minimalistic and sustainable geniuses!</i>
                   <br/>
                   ${emailDisclaimer}`;
 
     const requestBody = {
-      "from": "Rewaer <info@rewaer.com>",
+      "from": "Kinkograph <info@kinkograph.com>",
       "to": sendto,
-      "subject": "Rewaer.app | Reset your password with this link",
+      "subject": "Kinkograph.com | Reset your password with this link",
       "body": body,
       "key": process.env.MAILMAN_KEY
     };
@@ -137,18 +137,18 @@ exports.mailService = {
                   Feel free anytime to respond to this mail in order to contact us.<br/>
                   <br/>
                   This link will only be active for 24 hours. <br/>
-                  https://rewaer.com/emailverify/${emailVerifyToken}<br/>
+                  https://kinkograph.com/emailverify/${emailVerifyToken}<br/>
                   <br/>
-                  Rewaer App<br/>
+                  Kinkograph<br/>
                   <i>Only trees should get new
                   leaves every years.</i>
                   <br/>
                   ${emailDisclaimer}`;
 
     const requestBody = {
-      "from": "Rewaer <info@rewaer.com>",
+      "from": "Kinkograph <info@kinkograph.com>",
       "to": sendto,
-      "subject": "Rewaer.app | Confirm your email address with this link",
+      "subject": "Kinkograph.com | Confirm your email address with this link",
       "body": body,
       "key": process.env.MAILMAN_KEY
     };
