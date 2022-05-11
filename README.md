3a7d1f31fd6a73a5a17cd085766bb44c5dec6ece
vanStaen <clement.vanstaen@gmail.com>
2022-05-11 17:04:40 +0200

    service worker WIP

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # kinkograph.com
-Private photo gallery
+Photo gallery
 
 
 ### Help from the internet
 Gallery v1 based on css from https://css-tricks.com/seamless-responsive-photo-grid/
 Gallery v2 based on css from https://css-tricks.com/piecing-together-approaches-for-a-css-masonry-layout/
+
+A2HS (Add2HomeScreen) https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js").then(() => {
+    console.log("Service Worker Registered");
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
