782e413896b8ed50cc8fa3ed4805fa72d854b3dc
vanStaen <clement.vanstaen@gmail.com>
2022-05-13 17:12:57 +0200

    update REACT_APP_API_URL in SW.js

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -22,16 +22,16 @@ export default function register() {
   
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
+    const publicUrl = new URL(process.env.REACT_APP_API_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // Our service worker won't work if REACT_APP_API_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.REACT_APP_API_URL}/service-worker.js`;
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
