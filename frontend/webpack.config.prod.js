e5ff4629cb10e2eec532c43d4715a21e70d08074
vanStaen <clement.vanstaen@gmail.com>
2022-05-17 18:22:43 +0200

    fix web pack configuration files

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -25,10 +25,10 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: "public/black-heart-shaped.png" },
-        { from: "public/white-heart-128.jpg" },
-        { from: "public/white-heart-192.jpg" },
-        { from: "public/white-heart-256.jpg" },
-        { from: "public/white-heart-512.jpg" },
+        { from: "public/white-heart-128.png" },
+        { from: "public/white-heart-192.png" },
+        { from: "public/white-heart-256.png" },
+        { from: "public/white-heart-512.png" },
         { from: "public/manifest.webmanifest" },
       ],
     }),
