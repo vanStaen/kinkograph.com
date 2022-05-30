55247dc67343453e11d4f2fdc01bf33f9f663949
vanStaen <clement.vanstaen@gmail.com>
2022-05-30 11:16:49 +0200

    UX improv

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -12,8 +12,10 @@ export const Welcome = () => {
   return (
     <div className="welcome__main ">
       <LanguageDropDown />
-      <div className="App__title">&nbsp;kinkograph</div>
-      <Login loginWithCode={loginWithCode} />
+      <div className="welcome__container ">
+        <div className="App__title">&nbsp;kinkograph</div>
+        <Login loginWithCode={loginWithCode} />
+      </div>
       <FooterStartPage
         loginWithCode={loginWithCode}
         setLoginWithCode={setLoginWithCode}
